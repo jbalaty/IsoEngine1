d6ef761a8dac390c0c4ffc96271ed68dd783a202
Jiri Balaty <jiri.balaty@gmail.com>
2015-01-07 19:48:26 +0200

    New test project - top-front dungeon

---
+++ Assets/Scripts/TileComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EPrefabType {
+    Tile, Object, Decoration
+}
+
+public class TileComponent : MonoBehaviour {
+
+    public EPrefabType PrefabType = EPrefabType.Tile;
+    public bool IsWalkable = true;
+    public bool IsFlyable = true;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/UVOffset.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class UVOffset : MonoBehaviour
+{
+    public Vector2 TextureSize = new Vector2(8f, 11f);
+    public Vector2 TextureOffset = new Vector2(0f, 0f);
+
+    // Use this for initialization
+    void OnRenderObject()
+    {
+        renderer.material.mainTextureScale = (new Vector2(1 / TextureSize.x, 1 / TextureSize.y));
+        renderer.material.mainTextureOffset = (new Vector2(TextureOffset.x / TextureSize.x, TextureOffset.y / TextureSize.y));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
